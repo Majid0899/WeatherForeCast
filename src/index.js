3597e0a71170c5e31b32bf0cd1baad002e44a8d4
Majid0899 <majidkhan0899@gmail.com>
2025-07-19 13:16:40 +0530

    added forecast of 5 Days functionality

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ const button = document.querySelector("#currentLocation");
 
 let recentSearch = JSON.parse(localStorage.getItem("recentCities")) || [];
 
-
 let hasAddOption = false;
 
 /**
  * Fetch the current weather
  * return an object with details
- * Country,Date,Temperature,Wind,Humidity
+ * Country,Date,Temperature,Wind,Humidity,weather condition
  */
 
 async function fetchCurrentWeather(city) {
@@ -30,6 +29,36 @@ async function fetchCurrentWeather(city) {
     const date = res.location.localtime.split(" ")[0];
 
     return { country, date, temp, wind, humidity, city };
+  } catch (error) {
+    console.log("Data not fetched", error);
+  }
+}
+
+/**
+ * Fetch forecast of 5 Days
+ * return an object with details
+ * Date,Temperature,Wind,Humidity,weather condition
+ */
+
+async function fetchForecastWeather(city) {
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=6&aqi=no&alerts=no`;
+
+  try {
+    const data = await fetch(url);
+    const res = await data.json();
+
+    const forecasts = res.forecast.forecastday;
+    forecasts.shift(0);
+
+    const weather_data = forecasts.map((forecast) => {
+      const date = forecast.date;
+      const temp = forecast.day.avgtemp_c;
+      const wind = forecast.day.maxwind_mph;
+      const humidity = forecast.day.avghumidity;
+      const condition = forecast.day.condition.text;
+      return { date, temp, wind, humidity, condition };
+    });
+    return weather_data;
   } catch (error) {
     console.log("Data not fetched", error);
   }
@@ -65,6 +94,25 @@ async function addCityData() {
   }
 }
 
+/** SearchCity 5 Days forecast Event */
+searchCity.addEventListener("click", addForecastData);
+
+/*Add Forecast city data */
+async function addForecastData() {
+  console.log("Forecast");
+  const city = document.querySelector("#cityInput").value.trim();
+  if (!validateLocation(city)) {
+    document.querySelector("#inputError").classList.remove("hidden");
+  } else {
+    document.querySelector("#inputError").classList.add("hidden");
+
+    const data = await fetchForecastWeather(city);
+
+    console.log(data);
+    renderForeCastData(data);
+  }
+}
+
 /* Current Location Event*/
 button.addEventListener("click", addCurrentLocation);
 
@@ -94,14 +142,13 @@ function addOptions() {
   });
 }
 
-recentCities.addEventListener('change',addCityDrop)
+recentCities.addEventListener("change", addCityDrop);
 
 /*Add current location from drop down*/
-async function addCityDrop(){
-  const city=this.value;
-  const data=await fetchCurrentWeather(city);
-  renderCurrentData(data)
-  
+async function addCityDrop() {
+  const city = this.value;
+  const data = await fetchCurrentWeather(city);
+  renderCurrentData(data);
 }
 
 /** Utility Function */
@@ -130,7 +177,6 @@ async function showPosition(position) {
 
 /* Handle Current Position Error*/
 function showError(error) {
-
   let message = "";
   switch (error.code) {
     case error.PERMISSION_DENIED:
@@ -169,6 +215,81 @@ function renderCurrentData(data) {
   document.querySelector("#humidity").innerText = `${data.humidity} %`;
 }
 
+/*Render Forecast in weather Dasboard */
+function renderForeCastData(forecasts) {
+  let cards = document.querySelector("#forecastCards");
+
+  console.log(cards);
+  cards.innerText = "";
+
+  
+
+  forecasts.forEach((forecast) => {
+    const card = document.createElement("div");
+    card.classList.add("bg-gray-200", "rounded-lg", "hover:animate-wiggle");
+    card.innerHTML = `
+  
+    <!-- Weather Forecast Card  Start-->
+              <!-- Date -->
+              <div class="mt-2 text-gray-700">
+                <h2 class="px-2 text-2xl font-bold">${forecast.date}</h2>
+              </div>
+              <!-- Date End -->
+
+              <!-- Temperature Start -->
+              <div class="mt-2 text-blue-800 hover:animate-bounce flex lg:pr-3">
+                <p class="font-semibold pt-1 pl-1.5">Temperature</p>
+                <p
+                  id="foreCasttemperature"
+                  class="flex text-xl pt-1 pl-1.5 pb-1 lg:p-0"
+                >
+                  ${forecast.temp} °C
+                  <img
+                    id="forecasttemperatureIcon"
+                    src="../images/temperature.png"
+                    alt="Weather Icon"
+                    class="w-10 h-8 mb-1.5 pl-1"
+                  />
+                </p>
+              </div>
+              <!-- Wind start  -->
+              <div
+                class="mt-2 text-yellow-600 hover:animate-bounce flex lg:pr-3"
+              >
+                <p class="font-semibold pt-1 pl-1.5">Wind Speed</p>
+                <p id="forecastwindSpeed" class="flex text-xl pt-1 pl-1.5 pb-1">
+                  ${forecast.wind} m/s
+                  <img
+                    id="forecastwindIcon"
+                    src="../images/wind.png"
+                    alt="Weather Icon"
+                    class="w-10 h-8 mb-1.5 pl-1"
+                  />
+                </p>
+              </div>
+              <!-- Wind End -->
+              <!-- Humidity Start-->
+              <div class="mt-2 text-green-900 hover:animate-bounce px-2 flex">
+                <p class="font-semibold pt-1 pl-1">Humidity</p>
+                <p id="forecasthumidity" class="flex text-xl pt-1 pb-1 pl-1">
+                  ${forecast.humidity} %
+                  <img
+                    id="forecasthumidity"
+                    src="../images/humidity.png"
+                    alt="Weather Icon"
+                    class="w-10 h-8 mb-1.5 pl-1"
+                  />
+                </p>
+              </div>
+              <!-- Humidity End -->
+           
+            <!-- Weather Forecast Card End -->
+`;
+    cards.append(card);
+  });
+}
+
+/*Validate the City */
 function validateLocation(location) {
   const locationRegex = /^[a-zA-Z\s]+$/;
   if (!location.trim()) {
@@ -183,6 +304,7 @@ function validateLocation(location) {
   return true;
 }
 
+/*Saves to Local Storge */
 function saveToLocalStorage() {
   localStorage.setItem("recentCities", JSON.stringify(recentSearch));
 }
